eefcd86e0b93c93b1e0f46d466d91c93bdfa8c94
jeshan <j@jeshan.co>
2019-07-29 17:53:50 +0400

    add script to handle new provider boilerplate

---
+++ create-provider.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+provider=${1}
+if [[ -z "${provider}" ]]; then
+    echo "Specify provider as sole parameter. Value is like terraform-provider-xxx in the https://github.com/terraform-providers org"
+    exit 1
+fi
+provider_file=tfbridge/providers/${provider}.go
+
+cat > ${provider_file} <<EOL
+package main
+
+import (
+	"github.com/hashicorp/terraform/config"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-${provider}/${provider}"
+)
+
+//noinspection ALL
+func CreateProvider() (terraform.ResourceProvider, error) {
+	provider := ${provider}.Provider()
+	rawConfig, err := config.NewRawConfig(map[string]interface{}{})
+	if err != nil {
+		return nil, err
+	}
+	conf := terraform.NewResourceConfig(rawConfig)
+	err = provider.Configure(conf)
+	return provider, err
+}
+EOL
+
+touch tfbridge/real-tests/${provider}-tests.go
+
+cat << EOF
+Configuration file ${provider_file} created. Now,
+1. edit it to add custom provider settings.
+2. run "go get -d ./..." to download provider dependency
+3. confirm dependency version added in go.mod is the latest release found at https://github.com/terraform-providers/terraform-provider-${provider}/releases
+4. add provider to list of binaries built in build.sh
+5. Write test for ${provider} under at tfbridge/real-tests/${provider}-tests.go
+6. Add example provider configuration as custom resource in tfbridge-providers.yaml similar to those already there.
+EOF
