339044af51c3875f4c2e5263728160239e158d9b
jeshan <j@jeshan.co>
2019-07-29 17:06:18 +0400

    fix get tag

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 name=tfbridge-providers
-TAG=`git rev-parse HEAD`
+TAG=`git describe --tags`
 
 sam package --template-file ${name}.yaml --s3-bucket ${BUCKET} --s3-prefix releases/${TAG}/artefacts --output-template-file ${name}-packaged.yaml
 aws s3 cp ${name}-packaged.yaml s3://${BUCKET}/releases/${TAG}/templates/${name}.yaml
