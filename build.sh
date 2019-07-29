a4ad8e83ec870cf81b37022ff0133233cbf12fb2
jeshan <j@jeshan.co>
2019-07-29 14:56:58 +0400

    update build script

--- build.sh
+++ build.sh
@@ -5,15 +5,16 @@ set -e
 mkdir -p dist/
 rm -rf dist/*
 
+go get -d ./...
 echo "Building main package"
 go build -o dist/main tfbridge/lambda/main.go
 
 for name in http github digitalocean gitlab netlify azurerm aws ; do
     echo "Building plugin for ${name}"
-    go build -buildmode=plugin -o dist/${name}.so tfbridge/providers/${name}.go
+    time go build -buildmode=plugin -o dist/${name}.so tfbridge/providers/${name}.go
     echo "Packaging plugin for ${name}"
     cd dist/
-    zip -9 package-${name}.zip main ${name}.so
+    time zip package-${name}.zip main ${name}.so
     cd ..
     rm dist/${name}.so
 done
