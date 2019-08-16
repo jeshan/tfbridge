e3e888ef07d12f2a040da79acf27eae473de0808
jeshan <j@jeshan.co>
2019-08-16 14:14:09 +0400

    fix typo

--- build-plugins.sh
+++ build-plugins.sh
@@ -2,7 +2,7 @@
 
 for filename in tfbridge/providers/*.go; do
     name=`basename ${filename} .go`
-    $(cat download-dependencies.txt | grep terraform-providers-${name})
+    $(cat download-dependencies.txt | grep terraform-provider-${name})
     echo "Building plugin for ${name}"
     time go build -buildmode=plugin -o dist/${name}.so tfbridge/providers/${name}.go
     if [[ $? -ne 0 ]]; then
